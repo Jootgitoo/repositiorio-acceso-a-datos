233c76eb9601286403fde264cbc9dad5d1436519
Jorge <jorgehm260505@gmail.com>
2024-10-22 17:41:15 +0200

    todos los metodos comentados

--- resultado-1/FileDOMXml/src/main/java/com/mycompany/filedomxml/modelo/GestionContenidoDOM.java
+++ resultado-1/FileDOMXml/src/main/java/com/mycompany/filedomxml/modelo/GestionContenidoDOM.java
@@ -47,8 +47,8 @@ public class GestionContenidoDOM {
     //CONSTRUCTOR
     
     /**
-     * 
-     * @param nombre Nombre del nodo principal
+     * Creas un nuevo documento XML con un elemento raiz especificado por parametro
+     * @param nombre Nombre del nodo principal (elemento principal)
      */
     public GestionContenidoDOM(String nombre){
         try {
@@ -59,7 +59,7 @@ public class GestionContenidoDOM {
             //Sirve para construir el documento XML
             builder = factory.newDocumentBuilder();
             
-            //Creas un nuevo documento XML
+            //Creas y manipulas un nuevo documento XML
             DOMImplementation implementation = builder.getDOMImplementation();
             
             //Crea un documento XML 
@@ -107,7 +107,8 @@ public class GestionContenidoDOM {
         //.crateElement --> creamos un nodo llamado el valor de datoEmple
         Element dato = this.documento.createElement(datoEmple);
         
-        //.appendChild --> añade el nodo creado al documento
+        //.appendChild --> añade el nodo creado al documento 
+        //Al elemento padre le añades (.appendChild) el elemento llamado dato
         elementoPadre.appendChild(dato);
         
         return dato;
@@ -115,7 +116,7 @@ public class GestionContenidoDOM {
     
     
     /**
-     * Sirve para crear un nodo, con un texto, y un elemento padre
+     * Sirve para crear un nodo, con un texto, y añadirselo a un elemento padre
      * @param datoEmple Nombre del nodo que vas a crear
      * @param texto Contenido del nodo
      * @param raiz Elemento padre que va a tener
@@ -135,42 +136,80 @@ public class GestionContenidoDOM {
         raiz.appendChild(dato);
     }
     
+    
+    /**
+     * Método que se utilizará para transformar documentos XML
+     * @return 
+     */
     private Transformer preProcess(){
         
+        //Almacena la instancia del transformador
         Transformer transformer = null;
 
         
         try {
+            
+            //Crear un nuevo objeto Transformer
+            //Esto sirve para transformar documentos XML a otros formatos
             transformer = TransformerFactory.newInstance().newTransformer();
+            
         } catch (TransformerConfigurationException ex) {
             Logger.getLogger(GestionContenidoDOM.class.getName()).log(Level.SEVERE, null, ex);
         }
         
-        transformer.setOutputProperty(OutputKeys.INDENT, "yes");
+        
+        transformer.setOutputProperty //Se establece una propiedad de salida (esto sirve para que te "devuelva" el documento
+        (OutputKeys.INDENT, //Indicamos que el XML tiene q estar indentado
+                "yes"); //Como ponemos yes saldrá identado, para que no lo haga tienes que poner "no"
+        
+        //Returneamos el transformer
         return transformer;
         
     }
     
-    
+    /**
+     * Generamos un archivo XML con un nombre especificado
+     *  param nombreArchivo nombre del archivo donde se va a guardar la informacion
+     */
     public void generarArchivodelDOM(String nombreArchivo){
         try {
+            
+            //Source --> Un objeto que implemente esta interfaz contiene la información necesaria para actuar como fuente de entrada (fuente XML o instrucciones de transformación).
+            
+            
+            //Creamos un objeto Source utilizando DOM por lo tanto podemos utilizar el documento DOM como fuente de transformacion
             Source source = new DOMSource(this.documento);
+            
+            //Crea un nuevo archivo (en este caso va a tener la ruta donde se va a guardar, incluyendo el nombre)
+            //En nuestro caso ./resources/Empleados.xml
             Result salida = new StreamResult(new File(nombreArchivo));
             
-            preProcess().transform(source, salida);
+            
+            //Generamos el archivo XML en el sistema de archivos con el nombre especificado en nombreArchivo
+            preProcess() //Llamamos a preProcess para obtener un Tranformer utilizando el metodo .transform
+                    .transform(source, salida); //Nos convierte el Source en un documento Result 
             
         } catch (TransformerException ex) {
             Logger.getLogger(GestionContenidoDOM.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
     
+    /**
+     * Mostramos el documento por pantalla
+     */
     public void mostrarPantalla(){
         
         try {
+            
+            //Creamos el documento que vamos a devolver
             Source source = new DOMSource(this.documento);
+            
+            //Cremos una instancia Result que va a ser el documento modificado (en este caso imprimiremos por pantalla)
             Result salida = new StreamResult(System.out);
             
-            preProcess().transform(source, salida);
+            //Generamos el archivo XML en el sistema de archivos con el nombre especificado en nombreArchivo
+            preProcess() //Llamamos a preProcess para obtener un Tranformer utilizando el metodo .transform
+                    .transform(source, salida); //Nos convierte el Source en un documento Result 
             
         } catch (TransformerException ex) {
             Logger.getLogger(GestionContenidoDOM.class.getName()).log(Level.SEVERE, null, ex);
@@ -178,34 +217,48 @@ public class GestionContenidoDOM {
     }
     
     
+    /**
+     * 
+     * @param nombreArchivo 
+     */
     public void cargarArchivoEnMemoria(String nombreArchivo) {
         try {
-            this.documento = this.builder.parse(new File(nombreArchivo));
-            this.documento .getDocumentElement().normalize();
+            this.documento = this.builder //Para analiazar el archivo XML
+                    .parse //Convierte el contenido del XML en un obteo Document que se almacena en la variable "documento" de la clase
+                    (new File(nombreArchivo)); //Crea un objeto File que es el archivo llamado nombreArchivo
+            
+            
+            this.documento.getDocumentElement() //obtenemos el elemento raiz del documento XML
+                    .normalize(); //Llamamos a normalize el elemento raíz, que realiza una normalización del documento, eliminando nodos de texto vacíos y fusionando nodos de texto contiguos. Esto asegura que el documento esté en un estado consistente y bien estructurado.
             
         } catch (SAXException | IOException ex){
             Logger.getLogger(GestionContenidoDOM.class.getName()).log(Level.SEVERE, null, ex);
         }  
     }
             
-    
+    /**
+     * Consigo el elemento principal (nodo padre) del documento
+     * @return 
+     */
     public String getElementPrincipal(){
         return this.documento.getDocumentElement().getNodeName();
     }
     
     /**
-     * Va a devolver el valor de un elemento que nosotros queramos
-     * @param tag
-     * @param element
+     * Busca el primer nodo hijo de un elemento que coincide con el nombre de etiqueta proporcionado.
+     * Si encuentra un nodo, devuelve su valor; de lo contrario, devuelve null.
+     *
+     * @param tag Elemento del que va a sacar todos los elementos
+     * @param element Elemento padre, Donde nos vamos a meter a buscar entre sus hijos
      * @return 
      */
     private String getTagValue(String tag, Element element){
         
         //Busca los que estén dentro del elemento tag
         NodeList nodeList = element. //Crea una lista de nodos que sean hijos del elemento
-                getElementsByTagName(tag) //Saca todos los elementos contenidos en el padre y que tienen tag como elemento hijo
+                getElementsByTagName(tag) //Saca todos los elementos hijos llamados tag
                 .item(0). //Coge el primer valor que coincide con tag
-                getChildNodes(); //Coje los nodos hijos (ignora atributos, por ejemplo)
+                getChildNodes(); //Coge los nodos hijos (ignora atributos, por ejemplo)
         
         Node node = nodeList.item(0); //Creo un nodo con la informacion del nodo hijo
         
@@ -217,16 +270,20 @@ public class GestionContenidoDOM {
     }
     
     /**
-     * 
+     * Dentro de un nodo, hay muchos Empleado, pues extraigo la información de uno de ellos
      * @param node
-     * @return 
+     * @return Devuelve un empleado
      */
     private Empleado getEmpleado(Node node){ //Le estoy pasando un nodo, porque getEmpleados() devuelve una NodeList
         
-        //ESto es asi porque el metodo .getElementsByTagName devuelve una NodeList
+        //Creamos una instancia empleado donde almacenaremos los datos del empleado en cuestion
         Empleado emple = new Empleado();
         
-        if (node.getNodeType() == Node.ELEMENT_NODE){ //Vernificamos si el nodo es de tipo element (no tiene por que serlo)
+        //Se verifica si el tipo de nodo es ELEMENT_NODE. 
+        //Esto asegura que el nodo es un elemento XML (como un <empleado>), ya que no todos los nodos son elementos 
+        //(pueden ser nodos de texto, comentarios, etc.)
+        
+        if (node.getNodeType() == Node.ELEMENT_NODE){ 
             Element element = (Element) node; //Si lo es, convierte el nodo en un objeto element
             
             //El empleado que he creado antes, lo relleno con los datos del element
@@ -234,29 +291,42 @@ public class GestionContenidoDOM {
             emple.setApellido(getTagValue("apellido", element));
             emple.setDepartamento(Integer.parseInt(getTagValue("departamento", element)));
             emple.setSalario(Double.parseDouble(getTagValue("salario", element)));
+
         }
         return emple; //Devuelvo el empleado
     }
     
     
+    /**
+     * De un nodo padre saca una lista con todos los elementos Empleado que tenga
+     * @return Devuelve una lista de Empleado
+     */
     public List<Empleado> getEmpleados(){
-        List<Empleado> empleList = new ArrayList<Empleado>(); //Me creo mi lista empleados
-        //Me devuelve todos los Empleado que haya
-        NodeList nodeList = this.documento.getElementsByTagName("Empleado"); //Creo la nodelista donde cojo todos los element (nodos)
         
-        for(int i=0; i<nodeList.getLength(); i++){
-            empleList.add(getEmpleado(nodeList.item(i))); //Añado los nodos a la lista de empleados
+        //Me creo mi lista empleados
+        List<Empleado> empleList = new ArrayList<Empleado>(); 
+        
+        //Creo la nodelista donde me devuelve todos los Empleado que haya dentro del nodo padre
+        NodeList nodeList = this.documento.getElementsByTagName("Empleado"); 
+        
+        
+        for(int i=0; i<nodeList.getLength(); i++){ //Lo hago tantas veces como longitud de la lista
+            empleList.add(getEmpleado(nodeList.item(i))); //Añado cada Empleado a la lista
         }
+        
+        //Devuelvo la lista
         return empleList;
     }
       
-    //Ejercicio 1
+    /**
+     * Añadimos la etiqueta carga al nodo Empleado
+     */
     public void addCargo(){
       
         //Me devuelve todos los Empleado que haya
-        NodeList nodeList = this.documento.getElementsByTagName("Empleado"); //Creo una nodelista donde cojo todos los element (nodos)
+        NodeList nodeList = this.documento.getElementsByTagName("Empleado"); 
         
-        //Va a añadir un elemento llamado Cargo 
+        //Va a añadir un elemento llamado Cargo por cada "cajon" que tenga la nodeList
         for (int i=0; i<nodeList.getLength(); i++){
              
             //De la nodeList en la posicion de i compruebo que lo q haya sea un empleado y lo obtengo como Element donde posteriormente le añadiré el cargo y el valor de cargo
@@ -264,7 +334,6 @@ public class GestionContenidoDOM {
             
             
             //Creamos un elemento hijo llamado cargo
-        
             addNodoYTexto("Cargo", "Por especificar", empleadoTemporal);
  
         }
@@ -272,20 +341,31 @@ public class GestionContenidoDOM {
     }  
     
     
-    //Ejercicio 2
+    /**
+     * Quito un elemento del hijo empleado
+     * @param nombreElemento 
+     */
     public void quitarElmentoDeEmpleado(String nombreElemento){
-        //Sacamos la lista
+        
+        //Sacamos la lista de Empleado
         NodeList nodeList = this.documento.getElementsByTagName("Empleado");
         
-        //rrecorro todos los nodos y cojo el nodo actual
+        //Recorro la lista entera
         for(int i=0; i < nodeList.getLength(); i++){
             
+            //Cojo el cajón numero i de la lista
+            //Casteando para comprobar que estamos cogiendo un Element 
             Element empleElemento = (Element) nodeList.item(i);
             
-            //Busco el hijo que quiero eliminar
+            //Busco el hijo que quiero eliminar, es decir,
+            //Primero obtengo una lista de Empleado que los busco dentro de Empleado
+            //Posteriormente dentro de empleado busco nombreElemento (siguiente linea)
             NodeList elementos = empleElemento.getElementsByTagName(nombreElemento);
             
-            if(elementos.getLength() > 0){ //Si existe lo borro
+            //Este if solo puede ser 1 o 0
+            //1 --> Si contiene el nodo que queremos eliminar
+            //0 --> Si no lo contiene
+            if(elementos.getLength() > 0){ //Si contiene el elemento que quiero lo borro
                 Node elementoABorrar = elementos.item(0);
                 empleElemento.removeChild(elementoABorrar);
             }
@@ -297,16 +377,19 @@ public class GestionContenidoDOM {
     
     //Ejercicio 3
     /**
-     * 
-     * @param identificador 
-     * @param nuevoSalario 
+     * Modificamos el salario de un Empleado
+     * @param identificador identificador del empleado (forma de encontrar a un empleado en especifico)
+     * @param nuevoSalario Salario nuevo que va a tener el empleado
      */
     public void modificarSalarioEmpleado(long identificador, String nuevoSalario){
         
         //Cojo a los Empleado y lo meto a una nodeList
         NodeList nodeList = this.documento.getElementsByTagName("Empleado");
         
+        //Boolean para marcar cuando lo hemos encontrado al empleado
         boolean encontrado = false;
+        
+        //Contador
         int i = 0;
 
         
@@ -331,8 +414,8 @@ public class GestionContenidoDOM {
                 //Estamos diciendo guardame en una NodeList todo los Salario que tenga el empleadoTemp
                 NodeList salarioList = empleadoTemp.getElementsByTagName("Salario");
                 
-                //
-                if(salarioList.getLength() > 0){ //Si tiene un salario
+                
+                if(salarioList.getLength() > 0){ //Si tiene un salario (solo puede tener 1) por eso la Length solo puede ser 1 o 0
                     
                     //Obtengo de la nodeList la posicion 0 (en este caso la nodeList solo va a tener 1 elemento que va ser en la pos 0 y es el salario
                     salarioList.item(0)
@@ -356,6 +439,5 @@ public class GestionContenidoDOM {
         System.out.println("No encuentro al empleado");
         
     }
-           
-   
+
 }
