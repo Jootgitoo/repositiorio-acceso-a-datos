b7f1f665182bc6296ce6e3d519bae8005b5e178a
JorgeDam26 <165775297+JorgeDam26@users.noreply.github.com>
2024-10-23 09:48:47 +0200

    metodos de ayuda

---
+++ resultado-1/helped-metods/src/main/java/com/mycompany/helped/metods/HelpedMetods.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.helped.metods;
+
+/**
+ *
+ * @author JHM by Jorge Herrera Martín
+ */
+public class HelpedMetods {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
