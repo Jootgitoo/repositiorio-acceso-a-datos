72a2d83b4aec878888e5d5c18b9cc8d578b8593c
Jorge <jorgehm260505@gmail.com>
2024-12-08 12:02:08 +0100

    ejercicios resueltos

--- R3/ejercicios/proyectoJPA2/src/main/java/com/mycompany/proyectojpa2/ProyectoJPA2.java
+++ R3/ejercicios/proyectoJPA2/src/main/java/com/mycompany/proyectojpa2/ProyectoJPA2.java
@@ -11,6 +11,10 @@ import jakarta.persistence.Persistence;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.math.BigDecimal;
+import java.sql.Date;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Collection;
 import java.util.Iterator;
 import java.util.logging.Level;
@@ -25,6 +29,7 @@ public class ProyectoJPA2 {
     static EntityManagerFactory emfactory;
     static EntityManager entitymanager;
     static Departamentos departamento;
+    static Empleados empleado;
     static DepartamentosJpaController departamentosJpaController;
     
 
@@ -46,9 +51,29 @@ public class ProyectoJPA2 {
 //        
 //            departamentosJpaController.create(departamentos);
 
+//--------------------------------------------------------------------------------------------------------------------------------
+    //CLASSROOM --> OPERACIONES DIRECTAS CON OBJETOS --> PRACTICA EN CASA
             
-            leerUnRegistroRelacionado();
+            //EJERCICIO 1
+            //leerUnRegistroRelacionado();
 
+            //EJERCICIO 2
+            //borrarDatos();
+            
+            //EJERCICIO 3
+            //modificarSalarioDepartamentoDeEmpleado();
+
+            //EJERCICIO 4
+            //borrarEmpleado();
+            
+            //EJERCICIO 5
+            //insertarEmpleados();
+            
+//--------------------------------------------------------------------------------------------------------------------------------
+    //CLASSROOM --> OPERACIONES DIRECTAS CON OBJETOS --> PRACTICA EN CLASE            
+            
+            //subirSalario();
+            
             cierraFactory();
 
 
@@ -57,6 +82,8 @@ public class ProyectoJPA2 {
         }
         
     }
+//--------------------------------------------------------------------------------------------------------------------------------
+    //MÉTODOS FUERA DEL MAIN
     
     /**
      * Inicializo el factory
@@ -76,10 +103,10 @@ public class ProyectoJPA2 {
     
     
     /**
-     * Leo un registro de la bbdd
+     * Leo el registro que tenga el id pasado
+     * @param deptId id del departamento del cual se va ha leer el registro
      */
     public static void leerUnRegistro(short deptId){
-        
         
         departamento = entitymanager.find(Departamentos.class, deptId );
         
@@ -91,8 +118,11 @@ public class ProyectoJPA2 {
     }
     
     
+    
     /**
-     * Leo un registro que me lo bloquea, es decir, me impide hacerle cualquier cambio 
+     * Leo un departamento bloqueandolo, es decir, mientras que yo lo tenga se "bloquea" y nadie puede hacer cambios sobre este departamento
+     * @param id id del departamento que va ha leer
+     * @return devuelve el departamento leido
      */
     public Departamentos findDepartamentosBloqueando(Short id){
         
@@ -124,6 +154,7 @@ public class ProyectoJPA2 {
         }
     }
     
+    
     /**
      * Recargo los datos cargados. Sirve por si yo tengo un dato y lo modificas este metodo hace un "refresh" de ese departa
      */
@@ -136,25 +167,60 @@ public class ProyectoJPA2 {
     
     
     /**
-     * Mostramos el nombre del departamento y todos los empleados que contienen ese departamento
+     * Insertamos un departamento
+     * @param deptNo numero de departamento
+     * @param dNombre nombre del departamento
+     * @param dLocalidad  localidad del departamento a insertar
+     */
+    public static void insertarDatos(short deptNo, String dNombre, String dLocalidad){
+        Departamentos departamento;
+        
+        
+        departamento = new Departamentos();
+        departamento.setDeptNo( deptNo );
+        departamento.setDnombre(dNombre);
+        departamento.setLoc( dLocalidad );
+        
+        entitymanager.getTransaction().begin();
+        entitymanager.persist(departamento);
+        entitymanager.getTransaction().commit();
+    }
+    
+    /**
+     * Sirve para modificar los datos de un departamento
+     */
+    public static void modificarDatos(){
+        
+        entitymanager.getTransaction().begin();
+        
+        departamento = entitymanager.find(Departamentos.class, (short)50, LockModeType.PESSIMISTIC_READ );
+        
+        departamento.setLoc("Madrid");
+        esperar();
+        entitymanager.getTransaction().commit();
+       
+    }
+    
+//--------------------------------------------------------------------------------------------------------------------------------------
+    //CLASSROOM --> OPERACIONES DIRECTAS CON OBJETOS --> PRACTICA EN CASA
+    
+    /**
+     * Mostramos el nombre del departamento y todos los empleados que contienen ese departamento pedido por teclado
      * 
      * Con esto nos damos cuenta que al leer un departamento este departamento mete en una lista todos los empleados
-     * Esto viene en las clase Departamento (creadas por el id anteriormente)-
+     * Esto viene en las clase Departamento (creada por el id anteriormente)
      */
     public static void leerUnRegistroRelacionado(){
         try {
+            
             Empleados emple;
             
+            //Solicitamos el número de departamento
             BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-            
-            
             System.out.print("Indica el numero del departamento: ");
-            
             short nDep = Short.parseShort( br.readLine() );
             
-            
-            
-            //Leemos el departamento 10
+            //Leemos ese
             departamento = entitymanager.find(Departamentos.class, nDep );
             
             
@@ -185,59 +251,277 @@ public class ProyectoJPA2 {
         }
     }
     
+    
+    
     /**
-     * Insertamos un departamento
-     * @param deptNo numero de departamento
-     * @param dNombre nombre del departamento
-     * @param dLocalidad  localidad del departamento a insertar
+     * Borro los datos de un departamento pedido por teclado
      */
-    public static void insertarDatos(short deptNo, String dNombre, String dLocalidad){
-        Departamentos departamento;
-        
-        departamento = new Departamentos();
-        departamento.setDeptNo( deptNo );
-        departamento.setDnombre(dNombre);
-        departamento.setLoc( dLocalidad );
-        
-        entitymanager.getTransaction().begin();
-        entitymanager.persist(departamento);
-        entitymanager.getTransaction().commit();
-    }
-    
-    
-    public static void modificarDatos(){
-        
-        entitymanager.getTransaction().begin();
-        
-        departamento = entitymanager.find(Departamentos.class, (short)50, LockModeType.PESSIMISTIC_READ );
-        
-        departamento.setLoc("Madrid");
-        esperar();
-        entitymanager.getTransaction().commit();
-       
-    }
-    
-    
-    public static void borrarDatos(){
+    // DA ERROR PORQUE HAY EMPLEADOS RELACCIONADOS CON ESTE DEPARTAMENTO
+    //SI CREAS UN DEP NUEVO QUE NO TENGA EMPLEADOS Y LO BORRAS FUNCIONA
+    public static void borrarDepartamento(){
         
         try {   
+            
+            //Pedimos el numero del departamento al usuario
             BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-            
             System.out.print("Indica el numero del departamento: ");
-            
             short nDep = Short.parseShort( br.readLine() );
+          
+            //Realizamos la accion en la bbdd
+            entitymanager.getTransaction().begin();  
+            departamento = entitymanager.find(Departamentos.class, nDep, LockModeType.PESSIMISTIC_READ );
+            entitymanager.remove(departamento);
+            entitymanager.getTransaction().commit();
+            
+            
+        } catch (IOException ex) {
+            Logger.getLogger(ProyectoJPA2.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
+    
+    /**
+     * Te permite modificar el salario y el departamento de un empleado
+     */
+    public static void modificarSalarioDepartamentoDeEmpleado(){
+     
+        try {
+            
+            //Pedimos el numero del empleado
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            System.out.print("Indica el numero del empleado: ");
+            short nEmple = Short.parseShort( br.readLine() );
             
             
             entitymanager.getTransaction().begin();
             
-            departamento = entitymanager.find(Departamentos.class, (short)50, LockModeType.PESSIMISTIC_READ );
+            empleado = entitymanager.find(Empleados.class, nEmple, LockModeType.PESSIMISTIC_READ);
             
-            entitymanager.remove(departamento);
-            esperar();
+            if (empleado != null){ //Si el empleado es distinto de null, es decir, existe
+                
+                //Modificamos el salario
+                empleado.setSalario( new BigDecimal ("100") );
+                
+                //Modificamos el departamento
+                //Se hace así porque el setDeptNo recibe un departamento por parametro
+                Departamentos dep = new Departamentos();
+                dep.setDeptNo( (short) 10);
+                empleado.setDeptNo(dep);
+                
+                entitymanager.getTransaction().commit();
+                
+            } else { //Si el empleado es null, es decir, no existe
+                System.out.println("El empleado indicado no existe");
+            }
+            
+            
+        } catch (IOException ex) {
+            Logger.getLogger(ProyectoJPA2.class.getName()).log(Level.SEVERE, null, ex);
+        }   
+    }
+    
+    
+    /**
+     * Borra un empleado pedido por teclado
+     */
+    public static void borrarEmpleado(){
+        
+        try {
+            //Pedimos el numero del empleado
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            System.out.print("Indica el numero del empleado: ");
+            short nEmple = Short.parseShort( br.readLine() );
+            
+            //Abrimos la transaccion
+            entitymanager.getTransaction().begin();
+            
+            //Buscamos el empleado con dicho numero
+            empleado = entitymanager.find(Empleados.class, nEmple, LockModeType.PESSIMISTIC_READ);
+            
+            //Borramos el empleado
+            entitymanager.remove(empleado);
+            
+            //Guardamos el cambio
             entitymanager.getTransaction().commit();
+            
+        } catch (IOException ex) {
+            
+            Logger.getLogger(ProyectoJPA2.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
+    /**
+     * Inserta un empleado pasado pasado por teclado
+     */
+    public static void insertarEmpleados(){
+        
+        try {
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            
+            //Pedimos por teclado todos los datos
+            System.out.print("Indica el numero del empleado: ");
+            short nEmple = Short.parseShort( br.readLine() );
+            
+            System.out.print("Indica el apellido del empleado: ");
+            String apellido = br.readLine();
+            
+            System.out.print("Indica el oficio del empleado: ");
+            String oficio = br.readLine();
+            
+            System.out.print("Indica el director del empleado: ");
+            short dir = Short.parseShort( br.readLine() );
+            
+            System.out.print("Indica la fecha alta del empleado en formato (DD/MM/YYYY): ");
+            Date fechaAlta = convertirFecha( br.readLine() );
+            
+            System.out.print("Indica el salario del empleado: ");
+            String salario = br.readLine();
+            
+            System.out.print("Indica la comision del empleado: ");
+            String comision = br.readLine();
+            
+            System.out.print("Indica el numero del departamento al que pertenece: ");
+            String nDep = br.readLine();
+            departamento = new Departamentos();
+            departamento.setDeptNo( Short.parseShort(nDep));
+            
+            //Añadimos los datos al empleado que vamos a insertar
+            empleado = new Empleados();
+            empleado.setEmpNo(nEmple);
+            empleado.setApellido(apellido);
+            empleado.setOficio(oficio);
+            empleado.setDir(dir);
+            empleado.setFechaAlt(fechaAlta);
+            empleado.setSalario( new BigDecimal(salario) );
+            empleado.setComision( new BigDecimal(comision) );
+            empleado.setDeptNo(departamento );
+            
+            //Realizamos la accion
+            entitymanager.getTransaction().begin();
+            entitymanager.persist(empleado);
+            entitymanager.getTransaction().commit();
+           
+            
+            
         } catch (IOException ex) {
             Logger.getLogger(ProyectoJPA2.class.getName()).log(Level.SEVERE, null, ex);
         }
+
         
     }
+    
+    /**
+     * Sirve para convertir un String en un Date
+     * @param fecha decha que se va ha convertir en un Date
+     * @return la misma fecha pasada por parametro pero de tipo Date
+     */
+    private static Date convertirFecha(String fecha){
+        java.util.Date fechaUtil = null;
+        try {
+            SimpleDateFormat s = new SimpleDateFormat("DD/MM/YYYY");    
+            fechaUtil = s.parse(fecha);
+            
+        } catch (ParseException ex) {
+            Logger.getLogger(ProyectoJPA2.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return new java.sql.Date(fechaUtil.getTime());
+    }
+    
+//--------------------------------------------------------------------------------------------------------------------------------------
+    //CLASSROOM --> OPERACIONES DIRECTAS CON OBJETOS --> PRACTICA EN CASA
+    
+    /**
+     * Subimos el salario de los empleados que pertenezcan a un determinado departamento
+     */
+    public static void subirSalario(){
+        
+        Empleados emple;
+        try {
+            
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            
+            
+            System.out.print("Indica el numero del departamento: ");
+            short nDep = Short.parseShort( br.readLine() );
+            
+            System.out.print("Indica la subida en euros: ");
+            String subida = br.readLine();
+            
+            //Muestro los empleados y el salario de cada uno de un departamento antes de la subida
+            leerUnRegistroRelacionado(nDep);
+            esperar();
+            
+            //Hacemos el cambio
+            departamento = entitymanager.find(Departamentos.class, nDep);
+            
+            if (departamento != null){ //Si hay departamentos...
+            
+            //Coleccion que contiene todos los empleados de ese departamento
+            Collection<Empleados> listaEmpleados = departamento.getEmpleadosCollection();
+            
+            //Creamos un iterator para recorrer la coleccion
+            Iterator<Empleados> it = listaEmpleados.iterator();
+            
+            while( it.hasNext() ){ //Si el iterator tiene next, es decir, si tiene un empleado
+                
+                //Leemos el empleado
+                emple = it.next(); 
+                
+                //Cambiamos el salario
+                
+                emple.setSalario( emple.getSalario().add( new BigDecimal (subida) ) );
+            }
+            } else {
+                System.out.println("No existe el registro");
+            }
+            
+            //Muestro los empleados y el salario de cada uno de un departamento despues de la subida
+            leerUnRegistroRelacionado(nDep);
+            esperar();
+
+            
+        } catch (IOException ex) {
+            
+            Logger.getLogger(ProyectoJPA2.class.getName()).log(Level.SEVERE, null, ex);
+            
+        }
+    }
+    
+
+    /**
+     * Lee un departamento cuyo numero sea el pasado por parametro (contiene todos sus empleados)
+     * @param nDep numero de departamento que se va ha leer
+     */
+    private static void leerUnRegistroRelacionado(short nDep){
+        Empleados emple;
+        //Leemos el departamento
+        departamento = entitymanager.find(Departamentos.class, nDep );
+        if (departamento != null){ //Si hay departamentos...
+            
+            //Imprime el nombre del departamento
+            System.out.println("Nombre DEPARTAMENTO: " +departamento.getDnombre());
+            
+            //Coleccion que contiene todos los empleados de ese departamento
+            Collection<Empleados> listaEmpleados = departamento.getEmpleadosCollection();
+            
+            //Creamos un iterator para recorrer la coleccion
+            Iterator<Empleados> it = listaEmpleados.iterator();
+            
+            while( it.hasNext() ){ //Si el iterator tiene next, es decir, si tiene un empleado
+                
+                //Leemos el empleado
+                emple = it.next(); 
+                
+                //Imprimimos el nombre del empleado
+                System.out.print("--> Nombre EMPLEADO: " +emple.getApellido());
+                System.out.print(", SALARIO " +emple.getSalario());
+                System.out.println("");
+            }
+        } else {
+            System.out.println("No existe el registro");
+        }
+    }
+    
+    
+    
 }
