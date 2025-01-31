2af1d738d555ea24df9283fffc7ce92d7fb1824f
JorgeDam26 <jorgehm260505@gmail.com>
2025-01-31 10:57:15 +0100

    solucion tablas anidadas

---
+++ R4/ejercicios/objeto-relacional-tablasAnidadas/solucion-tablasAnidadas.sql
@@ -0,0 +1,14 @@
+--Realiza un procedimiento almacenado para insertar direcciones en la tabla
+--EJEMPLO_TABLA_ANIDADA.
+--• El procedimiento recibe como parámetros un identificador y un objeto
+--DIRECCION. Debe visualizar un mensaje indicando si se ha insertado o no la
+--dirección.
+--• Se deben hacer las siguientes comprobaciones y visualizar los mensajes
+--correspondientes:
+--• Comprobar si el identificador existe, si no existe es un caso de error,
+--visualizar el mensaje.
+--• Que la tabla anidada no sea null, si no es null hay que hacer un UPDATE
+--y no un INSERT.
+--• Que la dirección no exista ya en la tabla, si ya existe visualiza que no se
+--puede insertar.
+
